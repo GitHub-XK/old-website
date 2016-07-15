104442b7faab70edbb912be2e3869939f35bb419
SigvartBrendberg <walterhohman@gmail.com>
2016-07-15 22:13:44 +0200

    started raytracing project
    Former-commit-id: b71f6c9b20ee1ab9b800fc6efc579a7e70429527

--- js/dugout.js
+++ js/dugout.js
@@ -1,35 +1,49 @@
 var length = 50;
 var width = 30;
 var height = 15;
-var x = 0;
-var y = 0;
-var z = 0;
 var beamDensity = 3;
 var beamWidth = 0.5;
 var beamHeight = 6;
 var beamGap = 0.75;
 var results = [];
 /*
+draft:
 Case 1: ray hits nothing.
 Case 2: ray hits top of beam, going all the way to the bottom.
 Case 3: ray hits top of beam, escaping through the side of it, continuing to 1 or 5
 Case 4: ray hits side of beam, going all the way to the bottom.
 Case 5: ray hits side of beam, escaping through the side of it, continuing to 1 or 5
-NOTE! 1 must be BOTH a case and a function.
 */
 
 sideEscape = function(){
+	if(offsetZ + beamGap/velX < beamHeight){ //emitted through side
+		offsetZ += beamGap/velX;
+		sideHit();
+	};
 };
 
 sideHit = function(){
+	if(offsetZ + beamWidth/velX > beamHeight){ //escapes through bottom
+	}
+	else{
+		way += Math.sqrt(beamWidth*beamWidth * (1 + 1/velX));
+		offsetZ += beamWidth/velX;
+		sideEscape();
+	};
 };
 
-for(var i = 0; i<100;i++){
+for(var i = 0; i<100;i++){ //do a bunch of test cases
 	way = 0;
 //generate random beam
 	offsetX = Math.random()*(beamWidth+beamGap);
-	velX = 0; //? FIXME
-	velY = 0; //? FIXME
+	tryAgain = true;
+	while(tryAgain){
+		velX = 0; //? FIXME
+		velY = 0; //? FIXME
+		if(height * velX < length/2 && height * velY < width/2){
+			tryAgain = false;
+		};
+	};
 	offsetZ = 0;
 //
 	if(offsetX < beamWidth){
