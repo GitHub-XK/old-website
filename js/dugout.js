6cbd476cecba5be79c479e8ee65f941bb5e7bc71
SigvartBrendberg <walterhohman@gmail.com>
2016-07-15 21:59:59 +0200

    started raytracing project
    Former-commit-id: d670dc4e0f38354ea3fdd3fc49e0418764c74fe0

--- js/dugout.js
+++ js/dugout.js
@@ -15,22 +15,46 @@ Case 2: ray hits top of beam, going all the way to the bottom.
 Case 3: ray hits top of beam, escaping through the side of it, continuing to 1 or 5
 Case 4: ray hits side of beam, going all the way to the bottom.
 Case 5: ray hits side of beam, escaping through the side of it, continuing to 1 or 5
+NOTE! 1 must be BOTH a case and a function.
 */
 
-for(var i = 0; i<100;i++){
-	var way = 0;
-//generate random beam
-	var offsetX = Math.random()*(beamWidth+beamGap);
-	var velX = 0; //?
-	var velY = 0; //?
-	var offsetZ = 0;
-//
-	if(offsetX < beamWidth){
-		if(beamHeight*velX < beamWidth){
-			way
-		};
-	};
-	results.push(way);
+sideEscape = function(){
 };
 
+sideHit = function(){
+};
 
+for(var i = 0; i<100;i++){
+	way = 0;
+//generate random beam
+	offsetX = Math.random()*(beamWidth+beamGap);
+	velX = 0; //? FIXME
+	velY = 0; //? FIXME
+	offsetZ = 0;
+//
+	if(offsetX < beamWidth){
+		if(beamHeight*velX < beamWidth - offsetX){
+			//Case 4 (complete)
+			way = Math.sqrt(beamHeight * beamHeight * (velX + 1));
+		}
+		else{
+			//Case 3 (complete)
+			way += Math.sqrt((beamWidth - offsetX) * (beamWidth - offsetX) + ((beamWidth - offsetX)/velX) * ((beamWidth - offsetX)/velX));
+			offsetZ = (beamWidth - offsetX)/velX;
+			sideEscape();
+		};
+	}
+	else{
+		if(beamHeight*velX < beamGap - offsetX + beamWidth){
+			//Case 1 (complete)
+			way = 0;
+		}
+		else{
+			//Case 4 (kinda)
+			offsetZ = (beamGap - offsetX + beamWidth)/velX;
+			sideHit();
+		};
+	};
+	way = way * (Math.sqrt(1+velY*velY));
+	results.push(way);
+};
