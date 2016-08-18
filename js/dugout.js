e3fd55f5070cb647362caa53331b6af596d90b27
SigvartBrendberg <walterhohman@gmail.com>
2016-08-18 09:10:52 +0200

    commented code and wrote usage guide
    Former-commit-id: 1130d33fcee120d0cf55a1572e60849466e90bb3 [formerly 26ab53c09134d03f1dba933da3087ac27ef2e6c6]
    Former-commit-id: 29af9096b33538633917ad4a84ccf0a6be851508

--- js/dugout.js
+++ js/dugout.js
@@ -1,3 +1,23 @@
+/*
+ *Simulation of the radiation intensity in a beam covered dugout area.
+ *
+ *Disclaimer: Radiation is complicated.
+ *
+ *I am only using a simple model for shielding and creation of secondary radiation.
+ *In reality, radiation scatters, bends and bounce around. A completely accurate model would be extremely complicated.
+ *
+ *The results are only giving you a general idea of what shielding helps, and what does not.
+ *
+ *
+ *Details:
+ *
+ * - The metode used is a simplified form of ray tracing. This has the caveat of only giving a result for a small area instead of for a point. That is not good enough very close to the beam ceiling.
+ * - The simulation is performed for the centre of a rectangular hole. This is the location in the base with the most radiation.
+ *
+ *
+ */
+
+//change the following parameters to taste:
 //dimensions of the cuboid (in meters)
 var length = 50;
 var width = 30;
@@ -10,24 +30,16 @@ var beamHeight = 6;
 var beamGap = 0.75;
 //Do not edit after this point
 var results = [];
-/*
-draft:
-Case 1: ray hits nothing.
-Case 2: ray hits top of beam, going all the way to the bottom.
-Case 3: ray hits top of beam, escaping through the side of it, continuing to 1 or 5
-Case 4: ray hits side of beam, going all the way to the bottom.
-Case 5: ray hits side of beam, escaping through the side of it, continuing to 1 or 5
-*/
 
 sideEscape = function(){
-	if(offsetZ + beamGap/velX < beamHeight){ //emitted through the side
+	if(offsetZ + beamGap/velX < beamHeight){ //emitted from the side of a beam
 		offsetZ += beamGap/velX;
 		sideHit();
 	};
 };
 
-sideHit = function(){
-	if(offsetZ + beamWidth/velX > beamHeight){ //escapes through the bottom
+sideHit = function(){//hits the side of a beam
+	if(offsetZ + beamWidth/velX > beamHeight){ //escapes through the bottom of the beam
 		way += Math.sqrt(beamHeight*beamHeight * (1 + velX));
 	}
 	else{
@@ -37,7 +49,7 @@ sideHit = function(){
 	};
 };
 simnumber = 1000000;
-for(var i = 0; i<simnumber;i++){ //do a bunch of test cases
+for(var i = 0; i<simnumber;i++){ //track a lot of test rays
 	way = 0;
 //generate random beam
 	offsetX = Math.random()*(beamWidth+beamGap);
@@ -47,31 +59,27 @@ for(var i = 0; i<simnumber;i++){ //do a bunch of test cases
 		dec = Math.asin(Math.random());
 		velX = Math.sin(RA) * 1/Math.cos(dec);
 		velY = Math.cos(RA) * 1/Math.cos(dec);
-		if(height * velX < length/2 && height * velY < width/2){
+		if(height * velX < length/2 && height * velY < width/2){//only proceed with calculating the ray if it does not hit a wall
 			tryAgain = false;
 		};
 	};
 	offsetZ = 0;
 //
-	if(offsetX < beamWidth){
-		if(beamHeight*velX < beamWidth - offsetX){
-			//Case 4 (complete)
+	if(offsetX < beamWidth){//ray hits the top of a beam
+		if(beamHeight*velX < beamWidth - offsetX){//the ray goes through the entire beam before it enters the base
 			way = Math.sqrt(beamHeight * beamHeight * (velX + 1));
 		}
-		else{
-			//Case 3 (complete)
+		else{//the ray goes through the beam for a while, but then escapes out in between the beams
 			way += Math.sqrt((beamWidth - offsetX) * (beamWidth - offsetX) + ((beamWidth - offsetX)/velX) * ((beamWidth - offsetX)/velX));
 			offsetZ = (beamWidth - offsetX)/velX;
 			sideEscape();
 		};
 	}
 	else{
-		if(beamHeight*velX < beamGap - offsetX + beamWidth){
-			//Case 1 (complete)
+		if(beamHeight*velX < beamGap - offsetX + beamWidth){//the ray does not hit a beam at all
 			way = 0;
 		}
-		else{
-			//Case 4 (kinda)
+		else{//the ray goes between the beams initially, but then hits the side of one
 			offsetZ = (beamGap - offsetX + beamWidth)/velX;
 			sideHit();
 		};
@@ -81,7 +89,7 @@ for(var i = 0; i<simnumber;i++){ //do a bunch of test cases
 };
 average = 0;
 missing=0;
-for(i=0;i<simnumber;i++){
+for(i=0;i<simnumber;i++){//model for amplification and reduction of radiation intensity as a function of how much materials the rays have passed through
 	if(results[i]*100*beamDensity < 150){
 		average += 1 + 8*results[i]*100*beamDensity/150;
 	}
