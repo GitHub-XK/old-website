8b67690b2191f29e737e73f9798d7166e22df645
SigvartBrendberg <walterhohman@gmail.com>
2016-11-03 19:33:33 +0100

    Delete styling.js
    Former-commit-id: 4b5cd4f4eadb456022c1967be239063573d8538c

--- js/styling.js
+++
@@ -1,8 +0,0 @@
-styling = {
-	overlay1024High:"<path d=\"M512 512m-128 0q0 -128 128 -128q128 0 128 128q0 128 -128 128q-128 0 -128 -128\" fill=\"black\" stroke=\"rgb(0,255,0)\" stroke-width=\"8\"/><path d=\"M544 512m-64 -75l96 75l-96 75 l0 -150\" fill=\"rgb(0,255,0)\"/>",
-	overlay1024Low:"<path d=\"M512 512m-128 0q0 -128 128 -128q128 0 128 128q0 128 -128 128q-128 0 -128 -128\" fill=\"black\" stroke=\"rgb(255,200,0)\" stroke-width=\"8\"/><path d=\"M544 512m-64 -75l96 75l-96 75 l0 -150\" fill=\"rgb(255,200,0)\"/>",
-	overlay512High:"",
-	overlay512Low:"",
-	overlay256High:"",
-	overlay256Low:""
-}
