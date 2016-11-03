ae1b81204f42b9d53e446b0ef028a399081e5742
SigvartBrendberg <walterhohman@gmail.com>
2016-11-03 08:07:15 +0100

    Add files via upload
    Former-commit-id: 72c752e29bb12abd06ccbde0f6031cbd33c586ba

--- js/space.js
+++ js/space.js
@@ -1,27 +1,30 @@
 /*
- * The space object, a library for various calculators at moonwards.com
- * GPL notice:
- * @licstart
- *
- * Copyright (C) 2016  Sigvart Brendberg
- *
- * The JavaScript code in this page is free software: you can
- * redistribute it and/or modify it under the terms of the GNU
- * General Public License (GNU GPL) as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.  The code is distributed WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
- *
- * As additional permission under GNU GPL version 3 section 7, you
- * may distribute non-source (e.g., minimized or compacted) forms of
- * that code without the copy of the GNU GPL normally required by
- * section 4, provided you include this license notice and a URL
- * through which recipients can access the Corresponding Source.
- *
- * @licend  The above is the entire license notice
- * for the JavaScript code in this page.
- */
+The space object, a library for various calculators at moonwards.com
+GPL notice:
+@licstart
+    Copyright 2016 Sigvart Brendberg
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@licend
+*/
+/*
+Info:
+
+DEVELOPMENT VERSION! Things may change or not work
+All units are metric and unprefixed, unless otherwise noted.
+
+*/
 var space = {
 	earth:{
 		mass:7.342e22,
@@ -60,7 +63,8 @@ var space = {
 		}
 	},
 	hohmann:function(gm,r,R){
-		return Math.abs(vCirc(gm,r) - vElli(gm,r,r,R)) + Math.abs(vCirc(gm,R) - vElli(gm,R,r,R))
+		return Math.abs(vCirc(gm,r) - vElli(gm,r,r,R))
+			+ Math.abs(vCirc(gm,R) - vElli(gm,R,r,R))
 	},
 	vCirc:function(gm,r){
 		return Math.sqrt(gm/r)
@@ -70,5 +74,64 @@ var space = {
 	},
 	vEsc:function(gm,r){
 		return Math.sqrt(2*gm/r)
+	},
+	vDesc:function(gm,P,A){
+		return vEsc(gm,P) - vElli(gm,P,P,A)
+	},
+	optimalTransfer:function(gm,origin,target){
+		/*orbit object:{A:,P:,inc:,asc:,arg:}
+		A and P are mandatory, inc, asc and arg are optional*/
+		var deltaCost = vDesc(gm,origin.P,origin,A) + vDesc(gm,target.P,target.A);//you can always do an infinite apoapsis bi-elliptic transfer.
+		if(origin === target){//sometimes, we are lucky :P
+			return 0;
+		};
+		if(
+			origin.inc === undefined ||
+			target.inc == undefined ||
+			(origin.inc === target.inc && origin.asc === target.asc)
+		)//determine if the orbits are coplanar
+		{
+		}
+		else{
+		}
+	},
+	orbit:{
+		/*functions for orbits a {gm:,a:,A:,P:,inc:,asc:,arg:r,v:,vP:,vA:,ano:}
+			,where all properties are optional. The functions do as best they can.
+		*/
+		autocomplete:function(orbit){//deriving other properties from the existing ones.
+			var def = function(test){//shorthand for testing if a property is defined
+				if(test === udefined){
+					return false
+				}
+				return true
+			}
+			var newOrbit = orbit;//FIXME, proper object cloning
+			if(!def(orbit.a)){
+				if(def(orbit.P) && def(orbit.A){
+					newOrbit.a = (orbit.A + orbit.P)/2
+				}
+			};
+			if(!def(orbit.A)){
+				if(def(orbit.a) && def(orbit.P){
+					newOrbit.A = 2*orbit.a - orbit.P
+				}
+			};
+			if(!def(orbit.P)){
+				if(def(orbit.a) && def(orbit.A){
+					newOrbit.A = 2*orbit.a - orbit.A
+				}
+			};
+			if(!def(orbit.v)){
+				if(def(orbit.gm)){
+					if(def(orbit.r)){
+						if(def(orbit.a) && def(orbit.A){
+							newOrbit.v = vElli(orbit.gm,orbit.r,orbit.P,orbit.A)
+						}
+					}
+				}
+			};
+			return newOrbit
+		}
 	}
 }
