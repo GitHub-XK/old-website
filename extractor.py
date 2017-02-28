5bd16347ff9efa8b45563929785db7516e9858eb
SigvartBrendberg <sigvabrend@yandex.com>
2017-02-28 04:00:34 +0100

    completed extractor
    Former-commit-id: 41dbade52f511bcaa1041ff0443e365194e5a1ba

--- extractor.py
+++ extractor.py
@@ -4,17 +4,20 @@ import json
 
 with open("json/planets.json") as data_file:
 	data = json.load(data_file)
-for planets in data["planets"]:
-	f = open("_data/"+planets,"w")
-	try:
-		f.write("---")
-		f.write("name: "+planets)
-		f.write("mass: "+data[planets]["mass"])
-		f.write("radius: "+data[planets]["radius"])
-		f.write("gm: "+data[planets]["gm"])
-		f.write("parent: "+data[planets]["orbit"]["parent"])
-		f.write("semiMajor: "+data[planets]["orbit"]["semiMajor"])
-		f.write("period: "+data[planets]["orbit"]["period"])
-		f.write("---")
-	finally:
+f = open("_data/planets.yml","w")
+try:
+	for planets in data["planets"]:
+		f.write("- name: "+planets+"\n")
+		f.write("  mass: "+str(data[planets]["mass"])+"\n")
+		f.write("  radius: "+str(data[planets]["radius"])+"\n")
+		f.write("  gm: "+str(data[planets]["gm"])+"\n")
+		if "parent" in data[planets]["orbit"]:
+			f.write("  parent: "+data[planets]["orbit"]["parent"]+"\n")
+			f.write("  semiMajor: "+str(data[planets]["orbit"]["semiMajor"])+"\n")
+			f.write("  period: "+str(data[planets]["orbit"]["period"])+"\n")
+		else:
+			f.write("  parent: NaN\n")
+			f.write("  semiMajor: NaN\n")
+			f.write("  period: NaN\n")	
+finally:
    		f.close()
