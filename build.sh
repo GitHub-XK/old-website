e2bd8e097f6601777ff4094764673a64deefffc1
sigvart <sigvabrend@yandex.com>
2017-09-02 01:27:32 +0200

    always compile sass
    Former-commit-id: 25376a7428ebc8fcd2289d75bdc1b0d93f9f6d29 [formerly ed615c2d10820ab134f2720b77e17ec192283c8b]
    Former-commit-id: c1e1e7b9ff0b9dac4e4ef21ea93485b75cceecb8

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	ls -1 "$line" >> movedFiles.tmp
 	cp "$line"/* .
 done < "expandingDirectories.build"
+for i in sass/[!_]?*.sass; do
+	sass "$i" css${i//sass/}nest.sass.css --sourcemap=none;
+done
